b289505e0e84d74276635a0650532f789dcf2e4e
zemichaell <zemichael2014@gmail.com>
2023-10-10 13:15:48 -0700

    updated

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")(proces.env.SECRET_KEY);
+const stripe = require("stripe")(process.env.SECRET_KEY);
 //App config
 const app = express();
 
@@ -13,8 +13,8 @@ app.use(express.json());
 app.get("/", (request, response) => response.status(200).send("Hello Wold")),
   app.post("/payments/create", async (request, response) => {
     const total = request.query.total;
+    // console.log("Payment Request Recieved for this amount >>>", total);
     try {
-      console.log("Payment Request Recieved for this amount >>>", total);
       const paymentIntent = await stripe.paymentIntents.create({
         amount: parseInt(total),
         currency: "usd",
@@ -25,12 +25,15 @@ app.get("/", (request, response) => response.status(200).send("Hello Wold")),
         clientSecret: paymentIntent.client_secret,
       });
     } catch (error) {
-      console.log("Something went to wrong", error.message);
+      console.log(error.message);
+      response.status(500).send("something went wrong!");
     }
   });
 //listen connect
 // (express.api = functions.https.onRequest(app));
 
-app.listen(3003, () => {
-  if (!error) console.log("web server listening on port 3003");
+app.listen(3003, (err) => {
+  if (!err) {
+    console.log("server running on http://localhost:3003");
+  }
 });
